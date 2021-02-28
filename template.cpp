6c9c210dffabb3451df0bd8a9307d282a42fee32
Quynh Dinh <37500043+quynhdinh@users.noreply.github.com>
2021-02-28 12:18:31 +0700

    Add template.cpp

---
+++ template.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using pi = pair<int,int>;
+//io
+#define fast cin.tie(0); ios::sync_with_stdio(false)
+//
+#define str string
+#define debug(a) cerr << #a << ": " << a << endl
+#define fast cin.tie(0); ios::sync_with_stdio(false)
+// pairs
+#define mp make_pair
+#define f first
+#define s second
+// loops
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define rep(a) F0R(_,a)
+#define each(a,x) for (auto& a: x)
+// vectors
+// oops size(x), rbegin(x), rend(x) need C++17
+#define sz(x) int((x).size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend() 
+#define sor(x) sort(all(x)) 
+#define rsz resize
+#define ins insert 
+#define ft front()
+#define bk back()
+#define pb push_back
+#define eb emplace_back 
+#define pf push_front
+#define rtn return
+
+template<class T> bool ckmin(T& a, const T& b) {
+	return b < a ? a = b, 1 : 0; } // set a = min(a,b)
+
+template<class T> bool ckmax(T& a, const T& b) {
+	return a < b ? a = b, 1 : 0; }
+
+int32_t main(){ fast;
+
+}
