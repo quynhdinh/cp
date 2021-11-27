41d8434cd03f69912b1a18a4081ab6a2f94a8124
quynhdv <37500043+quynhdinh@users.noreply.github.com>
2021-11-27 11:44:45 +0700

    Remove macros in PrefixSums2D.h

--- content/data structures/Static Range Query/PrefixSums2D.h
+++ content/data structures/Static Range Query/PrefixSums2D.h
@@ -7,7 +7,7 @@
 template<class T> struct PrefixSums {
 	vector<vector<T>> sum;
 	void init(const vector<vector<T>>& v) {
-		int R = sz(v), C = sz(v[0]);
+		int R = v.size(), C = v[0].size();
 		sum.assign(R+1,vector<T>(C+1));
 		for(int i = 0; i < R; i++)  for(int j = 0; j < C; j++)
 			sum[i+1][j+1] = v[i][j]+sum[i+1][j]+sum[i][j+1]-sum[i][j];
