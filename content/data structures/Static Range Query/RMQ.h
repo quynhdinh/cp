a05e96a4dca94ca7a79f260c7669bcc6989e5bc7
quynhdv <37500043+quynhdinh@users.noreply.github.com>
2021-12-12 16:41:12 +0700

    remove macros rmq

--- content/data structures/Static Range Query/RMQ.h
+++ content/data structures/Static Range Query/RMQ.h
@@ -8,17 +8,19 @@
 	* https://pastebin.com/ChpniVZL
  * Memory: O(N\log N)
  * Time: O(1)
+ * Usage: RMQ<int> rmq; rmq.init(a); rmq.query(l, r);
  */
+
 
 template<class T> struct RMQ { // floor(log_2(x))
 	int level(int x) { return 31-__builtin_clz(x); }
 	vector<T> v; vector<vector<int>> jmp;
 	int comb(int a, int b) { // index of min
-		return v[a]==v[b]?min(a,b):(v[a]<v[b]?a:b); }
+		return v[a]==v[b]?min(a,b):(v[a]<v[b]?a:b); } // change this to any associative operation
 	void init(const vector<T>& _v) {
-		v = _v; jmp = {vector<int>(v.size())}; iota(all(jmp[0]),0);
+		v = _v; jmp = {vector<int>(v.size())}; iota(jmp[0].begin(), jmp[0].end(),0);
 		for (int j = 1; 1<<j <= v.size(); ++j) {
-			jmp.pb(vector<int>(v.size()-(1<<j)+1));
+			jmp.push_back(vector<int>(v.size()-(1<<j)+1));
 			for(int i = 0; i < jmp[j].size(); i++) jmp[j][i] = comb(jmp[j-1][i],
 									jmp[j-1][i+(1<<(j-1))]);
 		}
