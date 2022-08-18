40a0e0233da96f146986d29957df158ee1a7d998
quynhdv <quynhdv@vng.com.vn>
2022-08-18 08:30:45 +0700

    findNumberOfLIS

--- content/dp/lis.cpp
+++ content/dp/lis.cpp
@@ -33,3 +33,24 @@ return max;
 // https://leetcode.com/problems/maximum-height-by-stacking-cuboids/(harder)
 
 // fast : https://github.com/ngthanhtrung23/ACM_Notebook_new/blob/master/DP/lis.h
+
+
+// https://leetcode.com/problems/number-of-longest-increasing-subsequence
+int findNumberOfLIS(vector<int>& nums) {
+    int n = nums.size(), res = 0, max_len = 0;
+    vector<pair<int,int>> dp(n,{1,1});            //dp[i]: {length, number of LIS which ends with nums[i]}
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j <i ; j++){
+            if(nums[i] > nums[j]){
+                if(dp[i].first == dp[j].first + 1)dp[i].second += dp[j].second;
+                if(dp[i].first < dp[j].first + 1)dp[i] = {dp[j].first + 1, dp[j].second};
+            }
+        }
+        if(max_len == dp[i].first)res += dp[i].second;
+        if(max_len < dp[i].first){
+            max_len = dp[i].first;
+            res = dp[i].second;
+        }
+    }
+    return res;
+}
