c9cee06ae45f32e68a111f17b6a19dec0da37694
quynhdv <quynhdv@vng.com.vn>
2022-09-16 08:52:21 +0700

    weighted tree diameter

--- content/graph/tree/TreeDiameter.h
+++ content/graph/tree/TreeDiameter.h
@@ -29,3 +29,38 @@ template<int SZ> struct TreeDiameter {
 		center = {cen}; if (diaLen&1) center.push_back(par[cen]);
 	}
 };
+/*
+ * Description: Calculates longest path in tree and its path.
+ * Source: https://github.com/ngthanhtrung23/ACM_Notebook_new/blob/master/Graph/tree_diameter.h
+ * Verification:
+   * https://judge.yosupo.jp/problem/tree_diameter
+ */
+/*
+ * Usage: auto [dist, path] = get_diameter(g);
+ * */
+
+pair<long long, vector<int>> get_diameter(const vector<vector<pair<int, int>>> &g) {
+    int n = g.size();
+    vector<long long> dist(n);
+    vector<int> parent(n);
+
+    function<void(int, int, long long)> dfs = [&](int u, int fu, long long cur_dist) {
+        dist[u] = cur_dist;
+        parent[u] = fu;
+        for (auto [v, cost]: g[u])
+            if (v != fu) {
+                dfs(v, u, cur_dist + cost);
+            }
+    };
+    dfs(0, -1, 0);
+    // r = furthest node from root
+    int r = max_element(dist.begin(), dist.end()) - dist.begin();
+    dfs(r, -1, 0);
+    // r->s = longest path
+    int s = max_element(dist.begin(), dist.end()) - dist.begin();
+
+    vector<int> path;
+    for (int x = s; x >= 0; x = parent[x]) path.push_back(x);
+
+    return {dist[s], path};
+}
