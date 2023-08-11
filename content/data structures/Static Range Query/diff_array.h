dd9027ae7fd944a32da3c5334352ca1339ec8782
quynhdv <37500043+quynhdinh@users.noreply.github.com>
2023-08-11 13:25:49 +0700

    make diff_array.h backward compatible
    with earlier versions of cpp

--- content/data structures/Static Range Query/diff_array.h
+++ content/data structures/Static Range Query/diff_array.h
@@ -2,7 +2,10 @@ vector<int> diff_array(const vector<int>& a, const vector<tuple<int, int, int>>&
 	const int n = a.size();
 	vector<int> pref(n);
 	vector<int> result(n);
-	for(auto& [l, r, d]: ops){
+	for(const auto& tuple: ops){ // versions earlier than C++17 doesn't have that deep access, i.e: for(auto& [l, r, d]: ops){
+		int l = get<0>(tuple);
+		int r = get<1>(tuple);
+		int d = get<2>(tuple);
 		pref[l] += d;
 		if (r != n - 1)
 			pref[r + 1] -= d;
