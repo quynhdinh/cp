45727b5b6ace818e0d01af52067322cb915124a3
quynhdv <quynhdv@larion.com>
2024-05-30 14:27:06 +0700

    edit compress_array

--- content/data structures/Static Range Query/compress_array.h
+++ content/data structures/Static Range Query/compress_array.h
@@ -1,16 +1,24 @@
 // For an array A of size N, you want to compress so every number is 0 <= A[i] < N
 // References: https://codeforces.com/blog/entry/84164
-vector<int> compress(vector<int> a){
-  int n = a.size();
-  vector<pair<int, int>> pairs(n);
-  for(int i = 0; i < n; ++i) {
-  	pairs[i] = {a[i], i};
-  }
-  sort(pairs.begin(), pairs.end());
-  int nxt = 0;
-  for(int i = 0; i < n; ++i) {
-  	if(i > 0 && pairs[i-1].first != pairs[i].first) nxt++; // remove `&& pairs[i - 1].first != pairs[i].first` if you want to make a permutation
-  	a[pairs[i].second] = nxt;
-  }
-  return a;
+// permutation: return a permutation [0, n)
+template<typename T>
+vector<T> compress_array(vector<T> a, bool permutation){
+    const int n = (int)a.size();
+    vector<pair<T, int>> pairs(n);
+    for(int i = 0; i < n; ++i) {
+        pairs[i] = {a[i], i};
+    }
+    sort(pairs.begin(), pairs.end());
+    int nxt = 0;
+    for(int i = 0; i < n; ++i) {
+        if(i > 0){
+            if(!permutation && pairs[i - 1].first != pairs[i].first)
+                nxt++;
+            else {
+                nxt++;
+            }
+        }
+        a[pairs[i].second] = nxt;
+    }
+    return a;
 }
