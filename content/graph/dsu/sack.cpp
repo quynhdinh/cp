6a786d40a0b06d715b4a33d018be059e6124a3b1
Vu Quynh Dinh <37500043+quynhdinh@users.noreply.github.com>
2025-07-26 08:28:34 -0500

    Update sack.cpp
    Add CF - lomsat Gelral

--- content/graph/dsu/sack.cpp
+++ content/graph/dsu/sack.cpp
@@ -27,3 +27,77 @@ int main(){
     col.resize(n);
     return 0;
 }
+// CF - lomsat Gelral
+#include <bits/stdc++.h>
+#define REP(i, n) for (int i = 0, _n = (n); i < _n; i++)
+#define ll long long
+using namespace std;
+
+template<typename T_vector>
+void output_vector(const T_vector &v, bool add_one = false, int start = -1, int end = -1) {
+    if (start < 0) start = 0;
+    if (end < 0) end = (int)v.size();
+
+    for (int i = start; i < end; i++)
+        cout << v[i] + (add_one ? 1 : 0) << (i < end - 1 ? ' ' : '\n');
+}
+
+struct CNT {
+    map<int, int> cnt; // cnt[c]: the number of vertex in subtree has color c
+    int max_freq;
+    ll sum; // sum of those colors have max_freq
+
+    void init() {
+        cnt.clear();
+        max_freq = -1;
+        sum = 0;
+    }
+
+    void add(int c, int x) {
+        cnt[c] += x;
+        if (cnt[c] > max_freq) {
+            max_freq = cnt[c];
+            sum = c;
+        } else if (cnt[c] == max_freq)
+            sum += c;
+    }
+};
+int main() {
+    int n; cin>>n;
+    vector<int> colors(n), id(n);
+    vector<ll> res(n);
+    int nId = 0;
+    vector<CNT> cnt(n);
+    vector<vector<int>> g(n);
+    REP(i, n)
+        cin>>colors[i];
+    for(int i = 0; i < n - 1; i++){
+        int u, v; scanf("%d%d", &u, &v);
+        u--, v--;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    REP(i, n) cnt[i].init();
+    function<void(int, int)> dfs = [&](int u, int parent) {
+        int best = -1;
+        for(int v : g[u]) {
+            if (v == parent) continue;
+            dfs(v, u);
+            if (best < 0 || cnt[id[v]].cnt.size() > cnt[best].cnt.size())
+                best = id[v];
+        }
+        if (best < 0) // this is leaf
+            best = ++nId;
+        id[u] = best;
+        cnt[id[u]].add(colors[u], +1);
+        for(auto v : g[u]) {
+            if (v == parent) continue;
+            if (id[v] == id[u]) continue;
+            for(auto p : cnt[id[v]].cnt)
+                cnt[id[u]].add(p.first, p.second);
+        }
+        res[u] = cnt[id[u]].sum;
+    };
+    dfs(0, -1);
+    output_vector(res);
+}
