a1cd7886f0b00ca7d601e976505a1b5e36913f08
quynhdv <quynhvdinh@gmail.com>
2025-09-20 10:11:18 -0500

    improve output_vector and indent

--- content/miscellaneous/run_length_encoding.h
+++ content/miscellaneous/run_length_encoding.h
@@ -1,23 +1,19 @@
 // vector<pair<char, int>> runs = run_length_encoding<char>(S);
 template<typename T, typename T_iterable>
 vector<pair<T, int>> run_length_encoding(const T_iterable &items) {
-    vector<pair<T, int>> runs;
-    T previous = T();
-    int count = 0;
- 
-    for (const T &item : items)
-        if (item == previous) {
-            count++;
-        } else {
-            if (count > 0)
-                runs.emplace_back(previous, count);
- 
-            previous = item;
-            count = 1;
-        }
- 
-    if (count > 0)
+  vector<pair<T, int>> runs;
+  T previous = T();
+  int count = 0;
+  for (const T &item : items)
+    if (item == previous) {
+        count++;
+    } else {
+      if (count > 0)
         runs.emplace_back(previous, count);
- 
-    return runs;
+      previous = item;
+      count = 1;
+    }
+  if (count > 0)
+    runs.emplace_back(previous, count);
+  return runs;
 }
--- lc/contest_template.h
+++ lc/contest_template.h
@@ -1,7 +1,6 @@
 void __print(int x) {cout << x;} void __print(long x) {cout << x;} void __print(long long x) {cout << x;} void __print(unsigned x) {cout << x;} void __print(unsigned long x) {cout << x;} void __print(unsigned long long x) {cout << x;} void __print(float x) {cout << x;} void __print(double x) {cout << x;} void __print(long double x) {cout << x;} void __print(char x) {cout << '\'' << x << '\'';} void __print(const char *x) {cout << '\"' << x << '\"';} void __print(const string &x) {cout << '\"' << x << '\"';} void __print(bool x) {cout << (x ? "true" : "false");} template<typename T, typename V> void __print(const pair<T, V> &x) {cout << '{'; __print(x.first); cout << ','; __print(x.second); cout << '}';}template<typename T> void __print(const T &x) {int f = 0; cout << '{'; for (auto &i: x) cout << (f++ ? "," : ""), __print(i); cout << "}";} void _print() {cout << "]\n";} template <typename T, typename... V> void _print(T t, V... v) {__print(t); if (sizeof...(v)) cout << ", "; _print(v...);}
 #define debug(x...) cout << "[" << #x << "] = ["; _print(x)
-void db(const vector<bool>& v) { bool first = true; string res = "{"; for (int i = 0; i < int(v.size()); i++) { if (!first) res += ", "; first = false; res += to_string(v[i]); } res += "}"; cout<<res<<'\n';
-}
+void db(const vector<bool>& v) { bool first = true; string res = "{"; for (int i = 0; i < int(v.size()); i++) { if (!first) res += ", "; first = false; res += to_string(v[i]); } res += "}"; cout<<res<<'\n';}
 #define FOR(i, a, b) for (int i = (a), _b = (b); i <= _b; i++)
 #define FORD(i, b, a) for (int i = (b), _a = (a); i >= _a; i--)
 #define REP(i, n) for (int i = 0, _n = (n); i < _n; i++)
@@ -12,24 +11,24 @@ void db(const vector<bool>& v) { bool first = true; string res = "{"; for (int i
 #define fi   first
 #define se   second
 template<class X, class Y>
-    bool min_self(X &x, const Y &y) {
-        if (x > y) {
-            x = y;
-            return true;
-        } else return false;
-    }
+  bool min_self(X &x, const Y &y) {
+    if (x > y) {
+      x = y;
+      return true;
+    } else return false;
+  }
 template<class X, class Y>
-    bool max_self(X &x, const Y &y) {
-        if (x < y) {
-            x = y;
-            return true;
-        } else return false;
-    }
+  bool max_self(X &x, const Y &y) {
+    if (x < y) {
+      x = y;
+      return true;
+    } else return false;
+  }
 const int MOD = 1e9 + 7;
 inline void add_self(int& a, int val) {
-    if ((a = (a + val)) >= MOD)
-        a -= MOD;
+  if ((a = (a + val)) >= MOD)
+    a -= MOD;
 }
 inline void sub_self(int& a, int val) {
-    if ((a = (a - val)) < 0) a += MOD;
+  if ((a = (a - val)) < 0) a += MOD;
 }
